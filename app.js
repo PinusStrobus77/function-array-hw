c32abb6145063f8db7a9fd2c4d707b9e1a578906
Robert <rlhead94@gmail.com>
2023-02-10 14:10:29 -0500

    function hw

---
+++ app.js
@@ -0,0 +1,12 @@
+function addNum(x, y) {
+    console.log(x + y); 
+}
+function subNum(x, y){
+    console.log(x - y);
+}
+function multiplyNum (x, y){
+    console.log(x * y)
+}
+function divNum(x, y){
+    console.log(x / y)
+}
